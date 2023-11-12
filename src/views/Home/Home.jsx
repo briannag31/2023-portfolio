efa4465e9c9ac2725a75ee33abf56036666222f0
Brianna Gaines <bgaines@cisco.com>
2023-11-11 19:38:08 -0800

    adds to home screen

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+// import Card from '@mui/material/Card';
+// import CardContent from '@mui/material/CardContent';
 import './Home.css';
-import Container from '@mui/material/Container';
+// import Container from '@mui/material/Container';
 import headshot from '../../photos/headshot.png';
 export const Home = () => {
   return (
-    <Container>
-      <div className="circular--portrait">
-        <img src={headshot} />
+    <>
+      <div className="container">
+        <h1 className="home-text">
+          Hello, <br /> I&#x27;m Brianna!
+        </h1>
+        <div className="circular--portrait">
+          <img src={headshot} />
+        </div>
       </div>
-      <Card className="card">
-        <CardContent>hello</CardContent>
-      </Card>
-    </Container>
+      <div className="buttons">
+        <button className="button">About Me</button>
+        <button className="button">Resume</button>
+        <button className="button">My Projects</button>
+        <button className="button">Contact</button>
+      </div>
+    </>
   );
 };
