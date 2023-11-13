17026889925f58bab7d8807c94db385fe5d97b36
Brianna Gaines <bgaines@cisco.com>
2023-11-13 11:20:38 -0800

    homepage done

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,25 +1,34 @@
 import * as React from 'react';
-// import Card from '@mui/material/Card';
-// import CardContent from '@mui/material/CardContent';
+import Stack from '@mui/material/Stack';
+// import Button from '@mui/material/Button';
 import './Home.css';
-// import Container from '@mui/material/Container';
 import headshot from '../../photos/headshot.png';
 export const Home = () => {
   return (
     <>
       <div className="container">
-        <h1 className="home-text">
-          Hello, <br /> I&#x27;m Brianna!
-        </h1>
-        <div className="circular--portrait">
-          <img src={headshot} />
+        <div className="top">
+          <h1 className="home-text">
+            Hello, <br /> I&#x27;m Brianna!
+          </h1>
+          <div className="circular--portrait">
+            <img src={headshot} />
+          </div>
         </div>
-      </div>
-      <div className="buttons">
-        <button className="button">About Me</button>
-        <button className="button">Resume</button>
-        <button className="button">My Projects</button>
-        <button className="button">Contact</button>
+        <Stack spacing={2} direction="row" justifyContent="space-evenly" mt={20}>
+          <button className="button" href="/about">
+            <a href="/about"> About Me</a>
+          </button>
+          <button className="button">
+            <a href="/resume">Resume</a>
+          </button>
+          <button className="button" href="/projects">
+            <a href="/projects"> My Projects</a>
+          </button>
+          <button className="button" href="/contact">
+            <a href="/contact">Contact</a>
+          </button>
+        </Stack>
       </div>
     </>
   );
